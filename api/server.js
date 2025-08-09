9f7ca25ea7973c121fa3d4664b3f579e8a02b03f
vishnuvardhan1685 <vishnuvardhan1685@gmail.com>
2025-08-09 20:48:07 +0530

    path to regex

--- api/server.js
+++ api/server.js
@@ -35,7 +35,7 @@ app.use("/api/messages",messageRoutes)
 
 if(process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../client/dist")));
-    app.get("*", (req, res) => {
+    app.get("/*", (req, res) => {
         res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
     })
 }
